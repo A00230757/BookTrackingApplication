1199915ea0e1c0e0f66403e33faf856df5fcf57f
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-08-01 06:29:50 -0400

    All the Database models as written in the provided ERD diagram are created with all the properties mentioned. All models are implemented and have at least the Primary Keys marked with attributes. Also models have at least two data attributes per class.

---
+++ BookTrackingApplication/Data/CategoryType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookTrackingApplication.Data
+{
+    public class CategoryType
+    {
+        [Key]
+        [StringLength(80, MinimumLength = 1)]
+        [Display(Name = "Category Type")]
+        [RegularExpression("[A-Za-z]")]
+        public string Type { get; set; }
+
+
+        [StringLength(60, MinimumLength = 2)]
+        [Display(Name = "Category Type Name", Description = "Category Type  Name")]
+        [Required(ErrorMessage = "Category Type Name is Required", AllowEmptyStrings = false)]
+        [RegularExpression("[A-Za-z]")]
+        public string Name { get; set; }
+    }
+}
